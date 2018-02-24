f904f276bae5a6f8abf0d578cd7872ec4cc59c51
Walter Rothenberger <walter.rothenberger@gmail.com>
2018-02-24 19:46:29 +0100

    added first unit tests (figuring out dynamoDB later)

---
+++ unit_test.py
@@ -0,0 +1,13 @@
+import os
+os.environ["TELEGRAM_TOKEN"] = "509785164:AAF1MQSLMa1RMFJyFnFwbCc_4od26ZUGYOs"
+from handleReceivedMessages import parseTelegramMessage
+from distributeMessages import getChatIdsfromQueue
+def test_parseTelegramMessage():
+    body = {'message':{'text':'Test','chat':{'id':1,'first_name':'Testbert'},'reply_to_message':{'text':'ReplyToTest'}}}
+    result = {'message':'Test','chat_id':1,'first_name':'Testbert','reply_to_message_text':'ReplyToTest'}
+    assert parseTelegramMessage(body) == result
+
+def test_getChatIdsfromQueue():
+    amount,queue_in = 1,[11,22,33]
+    queue_out,chat_ids = [22,33,11],[11]
+    assert getChatIdsfromQueue(amount,queue_in) == (queue_out,chat_ids)
